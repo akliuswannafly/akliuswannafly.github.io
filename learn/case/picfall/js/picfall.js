93e6e25e3b55e777350360e67a74cbd95be08390
yzb <yzbwwjd@163.com>
2018-07-16 10:34:35 +0800

    add new function picfall

---
+++ learn/case/picfall/js/picfall.js
@@ -0,0 +1,7 @@
+for (var i = 1; i < 21; ++i) {
+  document.writeln("<div class='grid'>");
+  document.writeln("<div class='imgholder'>");
+  document.writeln("<img src='./images/" + i + ".jpg' alt='图片被盗走了' />");
+  document.writeln("</div>");
+  document.writeln("</div>");
+}
